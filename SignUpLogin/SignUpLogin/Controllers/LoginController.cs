7adb069e1ecde32443c08658c8c052959229adc4
rr-gillera <russellgillera19@gmail.com>
2026-02-27 01:26:11 +0800

    Yes

---
+++ SignUpLogin/SignUpLogin/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignUpLogin.Models;
+
+namespace SignUpLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login", new Login());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Add authentication logic here
+                // This will be implemented with database and authentication
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Login", model);
+        }
+    }
+}
